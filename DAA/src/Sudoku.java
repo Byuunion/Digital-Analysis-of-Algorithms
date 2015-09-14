7a3a69516488e7573ac2ab24f0384f4efb2f42c7
xub95 <xub95@RobiLab303-21.rowanads.rowan.edu>
2015-09-14 16:46:44 -0400

    Added ignore comment if

--- DAA/src/Sudoku.java
+++ DAA/src/Sudoku.java
@@ -1,5 +1,8 @@
 import java.io.*;
 
+/*
+ * Errors to fix: If file does not exist, fix 0 print outs
+ */
 public class Sudoku {
 
 	public static int width;
@@ -7,63 +10,64 @@ public class Sudoku {
 	public static int[][] array = new int[6][6];
 
 	public static void main(String[] args) {
-		readFile("C:/Users/BenX/git/Digital_Analysis_of_Algorithms/DAA/test");
+		readFile("H:/git/Digital-Analysis-of-Algorithms/DAA/test");
 	}
-	
-	public static void readFile(String fileName){
-		try {
-		FileReader inputFile = new FileReader(fileName);
-		BufferedReader bufferReader = new BufferedReader(inputFile);
-		String line;
-		int counter = 0;
-		int rowCounter = 0;
-		while ( (line = bufferReader.readLine()) != null ) {
-			String[] temp = line.split(" ");
-			if(temp[0] == "c"){
-				//System.out.println("Test");
-				continue;
-		}
-		if(counter == 0){
-			
-			width = Integer.parseInt(temp[0]);
-			
-		}
-		
-		else if(counter == 1){
-			height = Integer.parseInt(temp[0]);
-		}
-		
-		
-		else{
-			for(int i = 0; i < temp.length; i++){
-				array[rowCounter][i] = Integer.parseInt(temp[i]);
-			}
-		rowCounter++;
-			
-		}
-		
-		counter++;
-		
-		}
-				
-		
-		bufferReader.close();
-		
-		}
-		catch (IOException ex){
-	        System.out.println("Error: " + fileName);
-	    }
-		
-		
-		for(int i = 0; i < array.length; i++) {
-			String toString = "";
-			
-			for (int j = 0; j < array[i].length; j++) {
-				toString += array[i][j];
-			}
-			
-			System.out.println(toString);
 
+	public static void readFile(String fileName) {
+		try {
+			FileReader inputFile = new FileReader(fileName);
+			BufferedReader bufferReader = new BufferedReader(inputFile);
+			String line;
+			int counter = 0;
+			int rowCounter = 0;
+			while ((line = bufferReader.readLine()) != null) {
+
+				String temp2 = line.trim();
+
+				if (temp2.isEmpty() || temp2.startsWith("c")) {
+					/* ignore line */;
+				}
+
+				else {
+
+					String[] temp = line.split(" ");
+
+					if (counter == 0) {
+
+						width = Integer.parseInt(temp[0]);
+
+					}
+
+					else if (counter == 1) {
+						height = Integer.parseInt(temp[0]);
+					}
+
+					else {
+						for (int i = 0; i < temp.length; i++) {
+							array[rowCounter][i] = Integer.parseInt(temp[i]);
+						}
+						rowCounter++;
+					}
+				}
+
+				counter++;
+
+			}
+
+			bufferReader.close();
+			for (int i = 0; i < array.length; i++) {
+				String toString = "";
+
+				for (int j = 0; j < array[i].length; j++) {
+					toString += array[i][j];
+				}
+
+				System.out.println(toString);
+
+			}
+
+		} catch (IOException ex) {
+			System.out.println("Error: " + fileName);
 		}
 
 	}
